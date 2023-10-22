ad648b2941c672446e5a1ddd5216b0c76add345e
Crousus <johanneswenz98@gmail.com>
2023-10-22 16:20:23 +0200

    more work

--- src/app/api/v1/quote/route.ts
+++ src/app/api/v1/quote/route.ts
@@ -1,5 +1,34 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
+import { kv } from "@vercel/kv";
 
-export const GET = async (req: Request, res: Request) => {
-    return NextResponse.json({message: "Hello World!"})
+const quotes = process.env["QUOTES"]
+
+export const GET = async (req: Request) => {
+    let quoteList = quotes?.split(" | ")
+
+    quoteList = quoteList ? quoteList : ["No quotes found"]
+    // Extract Bearer token from Authorization header
+    const authHeader = req.headers.get("Authorization");
+    const authSplit = authHeader?.split(" ");
+
+    if(!authHeader || authSplit?.length != 2 || authSplit[0] !== "Bearer") {
+        return new Response("Invalid Authorization. Please use a bearer token", { status: 401 });
+    }
+
+    // Mock token validation
+    if (authSplit[1] !== "YOUR_SECRET_TOKEN") {
+        return new Response("Invalid token", { status: 401 });
+    }
+
+    await Cart();
+    // Return a random quote
+    const randomQuote = quoteList[Math.floor(Math.random() * quoteList.length)];
+    return NextResponse.json({ message: randomQuote });
+};
+
+
+
+async function Cart() {
+  const cart = await kv.get;
+  console.log(cart);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,8 +46,7 @@ export default function Home() {
             />
           </div>
           <div className="overflow-hidden">
-            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32  bg-image" >
-              <div className="fade-effect">
+            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32" >
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -116,7 +115,6 @@ export default function Home() {
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
                   </div>
-              </div>
               </div>
               </div>
             </div>
